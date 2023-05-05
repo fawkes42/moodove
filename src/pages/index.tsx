22d1a25b83b9627f93b053944db3d2c7b98037be
Pedro Tozzi <pedrogbtozzi@gmail.com>
2023-05-05 03:20:29 -0300

    Table Post and a router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,12 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { SignInButton, useUser } from "@clerk/nextjs";
 import { SignOutButton } from "@clerk/clerk-react";
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -14,6 +17,11 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>{!user.isSignedIn ? <SignInButton /> : <SignOutButton />}</div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
