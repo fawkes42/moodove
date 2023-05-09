9b7d7a48ca774c676f8c252f8661c69d6706665d
Pedro Tozzi <pedrogbtozzi@gmail.com>
2023-05-09 01:57:41 -0300

    fix relative time's text color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,11 +42,11 @@ const PostView = (props: PostWithAuthor) => {
         <div className="flex items-center gap-2 text-sm text-foreground">
           <div>{`@${author.username}`}</div>
           <span>·</span>
-          <div>
+          <span className="text-muted-foreground">
             {formatDistanceToNow(new Date(post.createdAt), {
               addSuffix: true,
             })}
-          </div>
+          </span>
         </div>
         <span>{post.content}</span>
       </div>
